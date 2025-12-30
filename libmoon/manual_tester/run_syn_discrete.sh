5f2ebbb17f83e4f485fa4ab2501608d6f57cb364
xzhang2523-c <xzhang2523-c@my.cityu.edu.hk>
2025-12-30 20:01:10 +0800

    first commit

--- libmoon/manual_tester/run_syn_discrete.sh
+++ libmoon/manual_tester/run_syn_discrete.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 problem=regression
 seed_num=1
-
+epoch=20
 # mgdaub random epo pmgda
 # preference-based: agg_ls agg_tche agg_pbi agg_cosmos, agg_softtche
 # set-based pmtl hvgrad moosvgd
@@ -17,10 +17,9 @@ do
   # for solver_name in mgdaub random epo pmgda agg_ls agg_tche agg_mtche agg_pbi agg_cosmos agg_softtche pmtl hvgrad moosvgd
   for solver_name in mgdaub random epo agg_ls agg_tche agg_pbi agg_cosmos pmtl gradhv moosvgd
   do
-    python "$SCRIPT_DIR/run_syn_discrete.py" --solver-name $solver_name --draw-fig False --epoch 1000 --problem-name $problem --seed-idx $seed
+    python "$SCRIPT_DIR/run_syn_discrete.py" --solver-name $solver_name --draw-fig False --epoch $epoch --problem-name $problem --seed-idx $seed
   done
 done
-
 
 
 sleep 100
